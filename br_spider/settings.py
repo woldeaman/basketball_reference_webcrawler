d5a48e4640a6c60ab61e2b2b4f0fb7b9d839b89c
woldeaman <woldeaman@zedat.fu-berlin.de>
2021-06-30 16:13:14 +0200

    changed settings to have csv output in chronological order

--- br_spider/settings.py
+++ br_spider/settings.py
@@ -21,6 +21,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
+DEPTH_PRIORITY = 1
+SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
+SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
