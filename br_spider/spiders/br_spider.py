6439d64d4b4fd7caaab787c22516bf2ecff5256c
woldeaman <woldeaman@zedat.fu-berlin.de>
2021-07-01 18:20:28 +0200

    moved helper function outside and changed formatting

--- br_spider/spiders/br_spider.py
+++ br_spider/spiders/br_spider.py
@@ -3,6 +3,11 @@
 import scrapy
 import datetime as dt
 
+def extract_datetime(raw_date, read_format="%a, %b %d, %Y %I:%M%p", save_format="%Y-%m-%dT%H:%M:%S"):
+        """Extract date or time in ISO-format. Check python datetime docs for formating options."""
+        date = dt.datetime.strptime(raw_date, read_format)
+        return date.strftime(save_format)
+
 class BR_Data_Spider(scrapy.Spider):
     name = "basketball-reference"
     year = dt.date.today().strftime("%Y")  # get current year for standart season
@@ -15,17 +20,13 @@ class BR_Data_Spider(scrapy.Spider):
     def parse(self, response):
         """Parsing page links for this season."""
         season_pages = response.xpath("//div[@class='filter']/*/a")  # selects all anchors for months of the season
-        yield from response.follow_all(season_pages, callback=self.parse_game_data)  # generate requests for all pages automatically    
+        yield from response.follow_all(season_pages, callback=self.parse_game_data)  # generate requests for all pages automatically
     
-    def _extract_datetime(raw_date, read_format="%a, %b %d, %Y %I:%M%p", save_format="%Y-%m-%dT%H:%M:%S"):
-        """Extract date or time in ISO-format. Check python datetime docs for formating options."""
-            date = dt.datetime.strptime(raw_date, read_format)
-            return date.strftime(save_format)
-            
     def parse_game_data(self, response):
         """Parsing game data from a single page."""
         games = response.xpath("//*/table[@id='schedule']/tbody/*")  # select all games data in the table
         for game in games:
+            # TODO: use get() instead of extract(), which returns single string instead of list
             # data is arranged on different levels, check HTML structure of table on website
             date = game.xpath("*[@data-stat='date_game']/a/text()").extract()[0]
             time_raw = game.xpath("*[@data-stat='game_start_time']/text()").extract()[0]
@@ -43,8 +44,8 @@ class BR_Data_Spider(scrapy.Spider):
             notes = notes[0] if len(notes) > 0 else None
 
             # scrape basic game information from page
-            yield {'date': self._extract_datetime(date_time),
-                   'day': self._extract_datetime(date_time, save_format="%A"),  # %A means weekday
+            yield {'date': extract_datetime(date_time),
+                   'weekday': extract_datetime(date_time, save_format="%A"),  # %A means weekday
                    'home_team': h_team,
                    'home_score': int(h_score),
                    'away_team': a_team,
@@ -55,5 +56,42 @@ class BR_Data_Spider(scrapy.Spider):
                    }
             
             # yield request for additional detailed game data
-            game_details = game.xpath("*/a[text()='Box Score']")
-            yield response.follow(game_details, callback=self.parse_game_details)
+    #         game_details = game.xpath("*/a[text()='Box Score']")
+    #         yield from response.follow_all(game_details, callback=self.parse_game_details)
+            
+    # def parse_game_details(self, response):
+    #     """Parsing more detailed information for each game."""
+    #     date_time = response.xpath("//*/*[@class='scorebox_meta']/div/text()").extract()[0]  # reading date/time
+    #     info_tables = response.xpath("//div[@class='table_container' and contains(@id, 'game-basic')]")  # gathering tables
+        
+    #     # cycle through home/away tables
+    #     for table in info_tables:
+    #         team = table.xpath("*/caption/text()").extract()[0].split("(")[0].strip()  # extract team name
+    #         player_rows = table.xpath("*/*/tr[th[@scope='row']]")[:-1]  # last row is game total stats, skipping it
+    #         # loop through all players, first five are starters
+    #         for i, player in enumerate(player_rows):
+    #             role = 'Starter' if i < 5 else 'Reserve'
+    #             name = player.xpath("*/a/text()").get()
+    #             stats = {stat.attrib['data-stat'].upper(): stat.xpath("text()").get() for stat in player.xpath("td")}
+                
+    #             # scrape player specific game data 
+    #             yield {'date': extract_datetime(date_time, read_format="%I:%M %p, %B %d, %Y"),
+    #                    'team': team,
+    #                    'player': name,
+    #                    'role': role,
+    #                    **stats,
+    #                    }
+                
+                
+            
+            
+            
+            
+        
+        
+        
+        
+        
+            
+            
+            
